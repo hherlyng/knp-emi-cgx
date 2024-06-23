41e7d766f96c086ddb15af9b54bbae56aa91a3cd
hherlyng <hherlyng@hotmail.com>
2024-06-23 18:04:56 +0200

    fix test config path

--- tests/test_L2_norm_potentials.py
+++ tests/test_L2_norm_potentials.py
@@ -14,7 +14,7 @@ def test_L2_norm_of_potentials():
 		"""
 		# Create problem and initialize ionic models
 		tags = {'intra': 1, 'extra': 2, 'boundary': 3, 'membrane': 4}
-		problem_square = ProblemKNPEMI(config_file='./test_config.yml')
+		problem_square = ProblemKNPEMI(config_file='./tests/test_config.yml')
 		HH = HH_model(problem_square)
 		ionic_models = [HH]
 		problem_square.init_ionic_model(ionic_models)
